eb63a3abcc4370e91c872e30804aefe42e9a11bf
Felipe Gimenez <lipe-gimenez@hotmail.com>
2018-01-18 13:10:49 -0200

    Add component 'Footer'

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+const Footer = (props) =>{
+    return(
+        <footer>
+            <p className="footer-links">
+                <a href="https://github.com/felipegimenez" target="_blank">Veja o código Github</a>
+                <span>  /  </span>
+                <a href="mailto:contact@sivadass.in" target="_blank">Need any help?</a>
+            </p>
+        </footer>
+    )
+};
+
+export default Footer;
